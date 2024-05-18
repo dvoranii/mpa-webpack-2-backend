04f02c5f960a76c8a43b3ad2f3a8d41baaa18cf7
Ildi Dvorani <dvoranii@ildidev.com>
2024-05-18 12:01:04 -0400

    added csrf-token endpoint

--- app.js
+++ app.js
@@ -3,13 +3,35 @@ import multer from "multer";
 import bodyParser from "body-parser";
 import { saveForm } from "./scripts/firebase.js";
 import { verifyRecaptcha } from "./scripts/recaptcha.js";
+import csurf from "csurf";
+import cookieParser from "cookie-parser";
 
 const multerMiddleware = multer().none();
 
+const csrfProtection = csurf({ cookie: true });
+
 export function appMiddleware(app) {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+
+  app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get("/csrf-token", csrfProtection, (req, res) => {
+    try {
+      const token = req.csrfToken();
+      console.log("CSRF token generated:", token);
+      res.json({ csrfToken: token });
+    } catch (error) {
+      console.error("Error generating CSRF token:", error);
+      res.status(500).json({ error: "Error generating CSRF token" });
+    }
+  });
 
   app.post("/submit-form", multerMiddleware, async (req, res) => {
     const { recaptcha_response, name, email, message } = req.body;
